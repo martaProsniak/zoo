1e18cb100d09ddd7fb441c642ce27816ff1ab48a
martaprosniak <martaprosniak@gmail.com>
2018-12-30 22:07:31 +0100

    Switch-case instruction in the main method

--- src/main/java/pl/mp/animals/AnimalsMain.java
+++ src/main/java/pl/mp/animals/AnimalsMain.java
@@ -1,5 +1,7 @@
 package pl.mp.animals;
 
+import java.util.Scanner;
+
 /**
  * Application main class.
  */
@@ -10,5 +12,43 @@ public class AnimalsMain {
      */
     public static void main(String[] args) {
         System.out.println("Welcome to the ZOO!");
+
+        printMenu();
+
+        //Create new Scanner object to reads user input
+        Scanner inputScanner = new Scanner(System.in);
+
+        boolean running = true;
+
+        while (running){
+            //Read next user command
+            int command = inputScanner.nextInt();
+
+            switch (command){
+                case 1:{
+
+                    break;
+                } case 2:{
+
+                    break;
+                }case 3: {
+                    running = false;
+                    break;
+                }default:{
+                    printMenu();
+                }
+            }
+
+
+
+        }
+
+    }
+    //Prints user menu
+    private static void printMenu(){
+        System.out.println("Choose action: \n" +
+                "1 - load file \n" +
+                "2 - save file \n" +
+                "3 - quit");
     }
 }
