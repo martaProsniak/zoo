cc67cf3043d79876ff8dc0422b645a485ae77eda
martaprosniak <martaprosniak@gmail.com>
2018-12-30 18:35:09 +0100

    Animal abstract class created

---
+++ src/main/java/pl/mp/animals/Animal.java
@@ -0,0 +1,33 @@
+package pl.mp.animals;
+
+/**
+ * Animal representation.
+ */
+public abstract class Animal {
+    /**
+     * Animal name.
+     */
+    private String name;
+    /**
+     * Animal age.
+     */
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public abstract void eat ();
+}
